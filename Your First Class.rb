dc7c42f8b3c85ada670ead38a4add08e96f34166
yankela <yankelamarante@gmail.com>
2017-04-05 17:50:28 -0400

    Update and rename project3 to Your First Class.rb

---
+++ Your First Class.rb
@@ -0,0 +1,13 @@
+class Person
+  attr_accessor :hair_color, :name
+
+def initialize(hair_color, name)
+  @hair_color = hair_color
+  @name = name
+end
+
+    def sing(fave_song)
+    @sing = fave_song
+
+  end
+end
